a97de3bd2c0a6d6239e8e687c6b25cb0511687b6
hevelatt <83212330+hevelatt@users.noreply.github.com>
2022-01-28 13:56:25 +0200

    Rajapintaa hyödyntävä ohjelma, 1. versio

---
+++ rajapinnat-ja-palvelut-2022/Koronatartunnat/Paaikkuna.Designer.cs
@@ -0,0 +1,85 @@
+﻿
+namespace Koronatartunnat
+{
+    partial class Paaikkuna
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBoxAlue = new System.Windows.Forms.ComboBox();
+            this.dateTimePickerTartuntapaiva = new System.Windows.Forms.DateTimePicker();
+            this.labelTartunnat = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // comboBoxAlue
+            // 
+            this.comboBoxAlue.FormattingEnabled = true;
+            this.comboBoxAlue.Location = new System.Drawing.Point(83, 83);
+            this.comboBoxAlue.Name = "comboBoxAlue";
+            this.comboBoxAlue.Size = new System.Drawing.Size(121, 23);
+            this.comboBoxAlue.TabIndex = 0;
+            this.comboBoxAlue.SelectedIndexChanged += new System.EventHandler(this.comboBoxAlue_SelectedIndexChanged);
+            // 
+            // dateTimePickerTartuntapaiva
+            // 
+            this.dateTimePickerTartuntapaiva.Location = new System.Drawing.Point(83, 166);
+            this.dateTimePickerTartuntapaiva.Name = "dateTimePickerTartuntapaiva";
+            this.dateTimePickerTartuntapaiva.Size = new System.Drawing.Size(200, 23);
+            this.dateTimePickerTartuntapaiva.TabIndex = 1;
+            this.dateTimePickerTartuntapaiva.ValueChanged += new System.EventHandler(this.dateTimePickerTartuntapaiva_ValueChanged);
+            // 
+            // labelTartunnat
+            // 
+            this.labelTartunnat.AutoSize = true;
+            this.labelTartunnat.Location = new System.Drawing.Point(400, 128);
+            this.labelTartunnat.Name = "labelTartunnat";
+            this.labelTartunnat.Size = new System.Drawing.Size(38, 15);
+            this.labelTartunnat.TabIndex = 2;
+            this.labelTartunnat.Text = "label1";
+            // 
+            // Paaikkuna
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelTartunnat);
+            this.Controls.Add(this.dateTimePickerTartuntapaiva);
+            this.Controls.Add(this.comboBoxAlue);
+            this.Name = "Paaikkuna";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBoxAlue;
+        private System.Windows.Forms.DateTimePicker dateTimePickerTartuntapaiva;
+        private System.Windows.Forms.Label labelTartunnat;
+    }
+}
+
---
+++ rajapinnat-ja-palvelut-2022/Koronatartunnat/Paaikkuna.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Koronatartunnat
+{
+    public partial class Paaikkuna : Form
+    {
+        HttpClient client = new HttpClient();
+
+        string apiOsoite =
+            @"https://sampo.thl.fi/pivot/prod/fi/epirapo/covid19case/fact_epirapo_covid19case.json";
+        string paivatRajoite = 
+            @"?row=dateweek20200101-509093L";
+        string tartunnatRajoiteAlku = 
+            @"?row=hcdmunicipality2020-445222&column=measure-444833&filter=dateweek20200101-";
+        string tartunnatRajoiteLoppu;
+
+        Dictionary<string, string> paivat;
+        string jsonTartunnat;
+        Dictionary<string, string> tartuntatapaukset;
+
+        public static string HaeVerkosta(HttpClient c, string osoite)
+        {
+            Task<string> t = c.GetStringAsync(osoite);
+            try
+            {
+                return t.Result;
+            }
+            catch (AggregateException ae)
+            {
+                // https://docs.microsoft.com/en-us/dotnet/api/system.aggregateexception?view=net-6.0#examples
+                ae.Handle((x) =>
+                {
+                    if (x is HttpRequestException) // This we know how to handle.
+                    {
+                        MessageBox.Show("Osoitetta ei pystytty tulkitsemaan. Tarkista verkkoyhteys."); // TODO: Yhtenäistä virheviestit.
+                        return true;
+                    }
+                    return false; // Let anything else stop the application.
+                });
+
+                return "";
+            }
+        }
+
+        public static string ErotaOlio(string jsonYlaOlio, string jsonOlioAvain, string jsonData)
+        {
+            // Olion avaimen etsintä alkaa ylä-olion sijainnista.
+            int ylaOlionSijainti = jsonData.IndexOf(jsonYlaOlio);
+            if (ylaOlionSijainti < 0) return "";
+
+            int haetunOlionSijainti = jsonData.IndexOf(jsonOlioAvain, ylaOlionSijainti) + jsonOlioAvain.Length;
+            if (haetunOlionSijainti < ylaOlionSijainti) return "";
+
+            // Olion esittely loppuu oikeaan aaltosulkeeseen.
+            // Otetaan myös aaltosulku mukaan (+1).
+            int haetunOlionLoppuSijainti = jsonData.IndexOf('}', haetunOlionSijainti) + 1;
+            if (haetunOlionLoppuSijainti < haetunOlionSijainti) return "";
+
+            return jsonData[haetunOlionSijainti..haetunOlionLoppuSijainti];
+        }
+
+        public Paaikkuna()
+        {
+            InitializeComponent();
+            // Haetaan päivät ja niiden solmutunnisteet ja sijoitetaan sanakirjaan paivat.
+            AlustaPaivat();
+            // Päivitetään tartuntatapaukset päivävalinnan perusteella (oletuksena tänään).
+            PaivitaTartuntatapaustiedot();
+            AlustaAluevalikko();
+        }
+
+        private void AlustaPaivat()
+        {
+            paivat = JsonSerializer.Deserialize<Dictionary<string, string>>(
+                ErotaOlio(@"""dateweek20200101"" : {", @"""label"":", 
+                HaeVerkosta(client, apiOsoite + paivatRajoite)));
+        }
+
+        private void PaivitaTartuntatapaustiedot()
+        {
+            string tartuntapaiva = dateTimePickerTartuntapaiva.Value.ToString("yyyy-MM-dd");
+            tartunnatRajoiteLoppu = paivat.FirstOrDefault(x => x.Value == tartuntapaiva).Key;
+
+            jsonTartunnat = HaeVerkosta(client,
+                apiOsoite + tartunnatRajoiteAlku + tartunnatRajoiteLoppu);
+            try
+            {
+                tartuntatapaukset = JsonSerializer.Deserialize<Dictionary<string, string>>(
+                    ErotaOlio(@"""measure"" : {", @"""value"":", jsonTartunnat));
+            }
+            catch
+            {
+                // TODO: Virheviesti, virheellinen päivämäärävalinta.
+            }
+        }
+
+        private void AlustaAluevalikko()
+        {
+            // Luodaan sanakirjat alueindekseistä ja -nimistä.
+            // Tiedetään että niissä on samat avaimet.
+            var alueIndeksit = JsonSerializer.Deserialize<Dictionary<string, int>>(
+                ErotaOlio(@"""hcdmunicipality2020"" : {", @"""index"":", jsonTartunnat));
+            var alueNimet = JsonSerializer.Deserialize<Dictionary<string, string>>(
+                ErotaOlio(@"""hcdmunicipality2020"" : {", @"""label"":", jsonTartunnat));
+            // Merkkijonotaulukko, johon varastoidaan alueiden nimet alueindeksien osoittamiin paikkoihin.
+            string[] alueet = new string[alueIndeksit.Count];
+            foreach (string sid in alueIndeksit.Keys)
+            {
+                alueet[alueIndeksit[sid]] = alueNimet[sid];
+            }
+            // Alustetaan aluevalikko alueiden nimillä.
+            // Myöhemmin voidaan hakea tartuntamäärät indeksin avulla.
+            comboBoxAlue.Items.AddRange(alueet);
+            // Valitaan viimeinen indeksi (Kaikki Alueet).
+            // Kutsuu comboBoxAlue_SelectedIndexChanged().
+            comboBoxAlue.SelectedIndex = comboBoxAlue.Items.Count - 1;
+        }
+
+        private void PaivitaTartuntaluku()
+        {
+            labelTartunnat.Text = tartuntatapaukset[comboBoxAlue.SelectedIndex.ToString()];
+        }
+
+        private void dateTimePickerTartuntapaiva_ValueChanged(object sender, EventArgs e)
+        {
+            PaivitaTartuntatapaustiedot();
+            PaivitaTartuntaluku();
+        }
+
+        private void comboBoxAlue_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            PaivitaTartuntaluku();
+        }
+    }
+}
