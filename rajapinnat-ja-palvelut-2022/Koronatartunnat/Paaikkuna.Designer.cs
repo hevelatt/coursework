e3b61119a748d87bb4378c9d7108855c4fa30fd3
hevelatt <83212330+hevelatt@users.noreply.github.com>
2022-01-28 20:14:13 +0200

    Arviotoiminto, virheen käsittely jne.

--- rajapinnat-ja-palvelut-2022/Koronatartunnat/Paaikkuna.Designer.cs
+++ rajapinnat-ja-palvelut-2022/Koronatartunnat/Paaikkuna.Designer.cs
@@ -32,20 +32,40 @@ namespace Koronatartunnat
             this.comboBoxAlue = new System.Windows.Forms.ComboBox();
             this.dateTimePickerTartuntapaiva = new System.Windows.Forms.DateTimePicker();
             this.labelTartunnat = new System.Windows.Forms.Label();
+            this.buttonNayta = new System.Windows.Forms.Button();
+            this.menuStripPaavalikko = new System.Windows.Forms.MenuStrip();
+            this.tiedostoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.päivitäVerkkoyhteysToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lopetaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.asetuksetToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.päivämäärävalintaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBoxArvaus = new System.Windows.Forms.TextBox();
+            this.labelVastaus = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.buttonArvaus = new System.Windows.Forms.Button();
+            this.labelVirhe = new System.Windows.Forms.Label();
+            this.menuStripPaavalikko.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // comboBoxAlue
             // 
+            this.comboBoxAlue.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.comboBoxAlue.FormattingEnabled = true;
-            this.comboBoxAlue.Location = new System.Drawing.Point(83, 83);
+            this.comboBoxAlue.Location = new System.Drawing.Point(12, 12);
+            this.comboBoxAlue.Margin = new System.Windows.Forms.Padding(12);
             this.comboBoxAlue.Name = "comboBoxAlue";
-            this.comboBoxAlue.Size = new System.Drawing.Size(121, 23);
+            this.comboBoxAlue.Size = new System.Drawing.Size(200, 23);
             this.comboBoxAlue.TabIndex = 0;
             this.comboBoxAlue.SelectedIndexChanged += new System.EventHandler(this.comboBoxAlue_SelectedIndexChanged);
             // 
             // dateTimePickerTartuntapaiva
             // 
-            this.dateTimePickerTartuntapaiva.Location = new System.Drawing.Point(83, 166);
+            this.dateTimePickerTartuntapaiva.Enabled = false;
+            this.dateTimePickerTartuntapaiva.Location = new System.Drawing.Point(12, 47);
+            this.dateTimePickerTartuntapaiva.Margin = new System.Windows.Forms.Padding(12, 0, 12, 12);
             this.dateTimePickerTartuntapaiva.Name = "dateTimePickerTartuntapaiva";
             this.dateTimePickerTartuntapaiva.Size = new System.Drawing.Size(200, 23);
             this.dateTimePickerTartuntapaiva.TabIndex = 1;
@@ -54,22 +74,167 @@ namespace Koronatartunnat
             // labelTartunnat
             // 
             this.labelTartunnat.AutoSize = true;
-            this.labelTartunnat.Location = new System.Drawing.Point(400, 128);
+            this.labelTartunnat.Location = new System.Drawing.Point(553, 53);
+            this.labelTartunnat.Margin = new System.Windows.Forms.Padding(12, 6, 12, 12);
             this.labelTartunnat.Name = "labelTartunnat";
-            this.labelTartunnat.Size = new System.Drawing.Size(38, 15);
+            this.labelTartunnat.Size = new System.Drawing.Size(0, 15);
             this.labelTartunnat.TabIndex = 2;
-            this.labelTartunnat.Text = "label1";
+            // 
+            // buttonNayta
+            // 
+            this.buttonNayta.Location = new System.Drawing.Point(553, 12);
+            this.buttonNayta.Margin = new System.Windows.Forms.Padding(12);
+            this.buttonNayta.Name = "buttonNayta";
+            this.buttonNayta.Size = new System.Drawing.Size(75, 23);
+            this.buttonNayta.TabIndex = 3;
+            this.buttonNayta.Text = "Näytä";
+            this.buttonNayta.UseVisualStyleBackColor = true;
+            this.buttonNayta.Click += new System.EventHandler(this.buttonNayta_Click);
+            // 
+            // menuStripPaavalikko
+            // 
+            this.menuStripPaavalikko.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tiedostoToolStripMenuItem,
+            this.asetuksetToolStripMenuItem});
+            this.menuStripPaavalikko.Location = new System.Drawing.Point(0, 0);
+            this.menuStripPaavalikko.Name = "menuStripPaavalikko";
+            this.menuStripPaavalikko.Size = new System.Drawing.Size(640, 24);
+            this.menuStripPaavalikko.TabIndex = 4;
+            this.menuStripPaavalikko.Text = "menuStrip1";
+            // 
+            // tiedostoToolStripMenuItem
+            // 
+            this.tiedostoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.päivitäVerkkoyhteysToolStripMenuItem,
+            this.lopetaToolStripMenuItem});
+            this.tiedostoToolStripMenuItem.Name = "tiedostoToolStripMenuItem";
+            this.tiedostoToolStripMenuItem.Size = new System.Drawing.Size(64, 20);
+            this.tiedostoToolStripMenuItem.Text = "Tiedosto";
+            // 
+            // päivitäVerkkoyhteysToolStripMenuItem
+            // 
+            this.päivitäVerkkoyhteysToolStripMenuItem.Name = "päivitäVerkkoyhteysToolStripMenuItem";
+            this.päivitäVerkkoyhteysToolStripMenuItem.Size = new System.Drawing.Size(181, 22);
+            this.päivitäVerkkoyhteysToolStripMenuItem.Text = "Päivitä verkkoyhteys";
+            this.päivitäVerkkoyhteysToolStripMenuItem.Click += new System.EventHandler(this.päivitäVerkkoyhteysToolStripMenuItem_Click);
+            // 
+            // lopetaToolStripMenuItem
+            // 
+            this.lopetaToolStripMenuItem.Name = "lopetaToolStripMenuItem";
+            this.lopetaToolStripMenuItem.Size = new System.Drawing.Size(181, 22);
+            this.lopetaToolStripMenuItem.Text = "Lopeta";
+            this.lopetaToolStripMenuItem.Click += new System.EventHandler(this.lopetaToolStripMenuItem_Click);
+            // 
+            // asetuksetToolStripMenuItem
+            // 
+            this.asetuksetToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.päivämäärävalintaToolStripMenuItem});
+            this.asetuksetToolStripMenuItem.Name = "asetuksetToolStripMenuItem";
+            this.asetuksetToolStripMenuItem.Size = new System.Drawing.Size(70, 20);
+            this.asetuksetToolStripMenuItem.Text = "Asetukset";
+            // 
+            // päivämäärävalintaToolStripMenuItem
+            // 
+            this.päivämäärävalintaToolStripMenuItem.Name = "päivämäärävalintaToolStripMenuItem";
+            this.päivämäärävalintaToolStripMenuItem.Size = new System.Drawing.Size(170, 22);
+            this.päivämäärävalintaToolStripMenuItem.Text = "Päivämäärävalinta";
+            this.päivämäärävalintaToolStripMenuItem.Click += new System.EventHandler(this.päivämäärävalintaToolStripMenuItem_Click);
+            // 
+            // textBoxArvaus
+            // 
+            this.textBoxArvaus.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxArvaus.Location = new System.Drawing.Point(12, 12);
+            this.textBoxArvaus.Margin = new System.Windows.Forms.Padding(12);
+            this.textBoxArvaus.Name = "textBoxArvaus";
+            this.textBoxArvaus.Size = new System.Drawing.Size(206, 23);
+            this.textBoxArvaus.TabIndex = 5;
+            // 
+            // labelVastaus
+            // 
+            this.labelVastaus.AutoSize = true;
+            this.labelVastaus.Location = new System.Drawing.Point(236, 53);
+            this.labelVastaus.Margin = new System.Windows.Forms.Padding(12, 6, 12, 12);
+            this.labelVastaus.Name = "labelVastaus";
+            this.labelVastaus.Size = new System.Drawing.Size(0, 15);
+            this.labelVastaus.TabIndex = 6;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.Controls.Add(this.comboBoxAlue, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.dateTimePickerTartuntapaiva, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.labelVastaus, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.buttonNayta, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.labelTartunnat, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 1, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 39);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(640, 87);
+            this.tableLayoutPanel1.TabIndex = 7;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.AutoSize = true;
+            this.tableLayoutPanel2.ColumnCount = 2;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel2.Controls.Add(this.textBoxArvaus, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.buttonArvaus, 1, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(224, 0);
+            this.tableLayoutPanel2.Margin = new System.Windows.Forms.Padding(0);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(317, 47);
+            this.tableLayoutPanel2.TabIndex = 7;
+            // 
+            // buttonArvaus
+            // 
+            this.buttonArvaus.Location = new System.Drawing.Point(230, 12);
+            this.buttonArvaus.Margin = new System.Windows.Forms.Padding(0, 12, 12, 12);
+            this.buttonArvaus.Name = "buttonArvaus";
+            this.buttonArvaus.Size = new System.Drawing.Size(75, 23);
+            this.buttonArvaus.TabIndex = 6;
+            this.buttonArvaus.Text = "Arvio";
+            this.buttonArvaus.UseVisualStyleBackColor = true;
+            this.buttonArvaus.Click += new System.EventHandler(this.buttonArvaus_Click);
+            // 
+            // labelVirhe
+            // 
+            this.labelVirhe.Dock = System.Windows.Forms.DockStyle.Top;
+            this.labelVirhe.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.labelVirhe.Location = new System.Drawing.Point(0, 24);
+            this.labelVirhe.Name = "labelVirhe";
+            this.labelVirhe.Size = new System.Drawing.Size(640, 15);
+            this.labelVirhe.TabIndex = 8;
+            this.labelVirhe.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // Paaikkuna
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.labelTartunnat);
-            this.Controls.Add(this.dateTimePickerTartuntapaiva);
-            this.Controls.Add(this.comboBoxAlue);
+            this.ClientSize = new System.Drawing.Size(640, 126);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.labelVirhe);
+            this.Controls.Add(this.menuStripPaavalikko);
+            this.MainMenuStrip = this.menuStripPaavalikko;
             this.Name = "Paaikkuna";
             this.Text = "Form1";
+            this.menuStripPaavalikko.ResumeLayout(false);
+            this.menuStripPaavalikko.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -80,6 +245,19 @@ namespace Koronatartunnat
         private System.Windows.Forms.ComboBox comboBoxAlue;
         private System.Windows.Forms.DateTimePicker dateTimePickerTartuntapaiva;
         private System.Windows.Forms.Label labelTartunnat;
+        private System.Windows.Forms.Button buttonNayta;
+        private System.Windows.Forms.MenuStrip menuStripPaavalikko;
+        private System.Windows.Forms.ToolStripMenuItem tiedostoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem asetuksetToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBoxArvaus;
+        private System.Windows.Forms.Label labelVastaus;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label labelVirhe;
+        private System.Windows.Forms.ToolStripMenuItem päivitäVerkkoyhteysToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem lopetaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem päivämäärävalintaToolStripMenuItem;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Button buttonArvaus;
     }
 }
 
--- rajapinnat-ja-palvelut-2022/Koronatartunnat/Paaikkuna.cs
+++ rajapinnat-ja-palvelut-2022/Koronatartunnat/Paaikkuna.cs
@@ -1,11 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
 using System.Net.Http;
-using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -27,47 +23,75 @@ namespace Koronatartunnat
         Dictionary<string, string> paivat;
         string jsonTartunnat;
         Dictionary<string, string> tartuntatapaukset;
+        int tartuntojenLkm;
 
-        public static string HaeVerkosta(HttpClient c, string osoite)
+        #region Tapahtumakäsittelijät
+
+        private void lopetaToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            Task<string> t = c.GetStringAsync(osoite);
-            try
-            {
-                return t.Result;
-            }
-            catch (AggregateException ae)
-            {
-                // https://docs.microsoft.com/en-us/dotnet/api/system.aggregateexception?view=net-6.0#examples
-                ae.Handle((x) =>
-                {
-                    if (x is HttpRequestException) // This we know how to handle.
-                    {
-                        MessageBox.Show("Osoitetta ei pystytty tulkitsemaan. Tarkista verkkoyhteys."); // TODO: Yhtenäistä virheviestit.
-                        return true;
-                    }
-                    return false; // Let anything else stop the application.
-                });
+            Application.Exit();
+        }
 
-                return "";
+        private void päivitäVerkkoyhteysToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            AlustaVirhe();
+
+            if (paivat == null) AlustaPaivat();
+            if (string.IsNullOrEmpty(jsonTartunnat)) PaivitaTartuntatapaustiedot();
+            if (comboBoxAlue.Items.Count == 0) AlustaAluevalikko();
+        }
+
+        private void päivämäärävalintaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (päivämäärävalintaToolStripMenuItem.Checked)
+            {
+                päivämäärävalintaToolStripMenuItem.Checked = false;
+                dateTimePickerTartuntapaiva.Enabled = false;
+            }
+            else
+            {
+                päivämäärävalintaToolStripMenuItem.Checked = true;
+                dateTimePickerTartuntapaiva.Enabled = true;
             }
         }
 
-        public static string ErotaOlio(string jsonYlaOlio, string jsonOlioAvain, string jsonData)
+        private void comboBoxAlue_SelectedIndexChanged(object sender, EventArgs e)
         {
-            // Olion avaimen etsintä alkaa ylä-olion sijainnista.
-            int ylaOlionSijainti = jsonData.IndexOf(jsonYlaOlio);
-            if (ylaOlionSijainti < 0) return "";
-
-            int haetunOlionSijainti = jsonData.IndexOf(jsonOlioAvain, ylaOlionSijainti) + jsonOlioAvain.Length;
-            if (haetunOlionSijainti < ylaOlionSijainti) return "";
-
-            // Olion esittely loppuu oikeaan aaltosulkeeseen.
-            // Otetaan myös aaltosulku mukaan (+1).
-            int haetunOlionLoppuSijainti = jsonData.IndexOf('}', haetunOlionSijainti) + 1;
-            if (haetunOlionLoppuSijainti < haetunOlionSijainti) return "";
-
-            return jsonData[haetunOlionSijainti..haetunOlionLoppuSijainti];
+            AlustaVirhe();
+            AlustaTartuntaluku();
         }
+
+        private void dateTimePickerTartuntapaiva_ValueChanged(object sender, EventArgs e)
+        {
+            AlustaVirhe();
+            PaivitaTartuntatapaustiedot();
+            AlustaTartuntaluku();
+        }
+
+        private void buttonArvaus_Click(object sender, EventArgs e)
+        {
+            AlustaVirhe();
+            if (int.TryParse(textBoxArvaus.Text, out int arvaus))
+            {
+                if (arvaus < tartuntojenLkm)
+                    labelVastaus.Text = "Tartuntojen lukumäärä suurempi kuin arvio";
+                if (arvaus > tartuntojenLkm)
+                    labelVastaus.Text = "Tartuntojen lukumäärä pienempi kuin arvio";
+                if (arvaus == tartuntojenLkm)
+                    labelVastaus.Text = "Tartuntojen lukumäärä sama kuin arvio";
+            }
+            else
+            {
+                NaytaVirhe("Arvio ei kokonaisluku", false);
+            }
+        }
+
+        private void buttonNayta_Click(object sender, EventArgs e)
+        {
+            NaytaTartuntaluku();
+        }
+
+        #endregion
 
         public Paaikkuna()
         {
@@ -76,9 +100,10 @@ namespace Koronatartunnat
             AlustaPaivat();
             // Päivitetään tartuntatapaukset päivävalinnan perusteella (oletuksena tänään).
             PaivitaTartuntatapaustiedot();
+            // Alustaa myös virheen ja tartuntaluvun.
             AlustaAluevalikko();
         }
-
+        
         private void AlustaPaivat()
         {
             paivat = JsonSerializer.Deserialize<Dictionary<string, string>>(
@@ -88,8 +113,24 @@ namespace Koronatartunnat
 
         private void PaivitaTartuntatapaustiedot()
         {
+            // Tarkasteltava päivä määräytyy päivävalinnan mukaan.
             string tartuntapaiva = dateTimePickerTartuntapaiva.Value.ToString("yyyy-MM-dd");
-            tartunnatRajoiteLoppu = paivat.FirstOrDefault(x => x.Value == tartuntapaiva).Key;
+
+            // Nollataan osoitteen loppuosa.
+            tartunnatRajoiteLoppu = "";
+            foreach (string sid in paivat.Keys)
+            {
+                // Jos avainta vastaava päivä on tartuntapäivä,
+                // niin asetetaan osoitteen loppuosaksi avain (solmutunniste).
+                if (paivat[sid].Equals(tartuntapaiva))
+                    tartunnatRajoiteLoppu = sid;
+            }
+            // Tilanne, jossa ei löytynyt päivää vastaavaa solmutunnistetta.
+            if (string.IsNullOrEmpty(tartunnatRajoiteLoppu))
+            {
+                NaytaVirhe("Virheellinen päivämäärä: ei pandemian ajalta", true);
+                return;
+            }
 
             jsonTartunnat = HaeVerkosta(client,
                 apiOsoite + tartunnatRajoiteAlku + tartunnatRajoiteLoppu);
@@ -98,14 +139,15 @@ namespace Koronatartunnat
                 tartuntatapaukset = JsonSerializer.Deserialize<Dictionary<string, string>>(
                     ErotaOlio(@"""measure"" : {", @"""value"":", jsonTartunnat));
             }
-            catch
+            catch (JsonException)
             {
-                // TODO: Virheviesti, virheellinen päivämäärävalinta.
+                NaytaVirhe("Virheellinen päivämäärä: ei tietoja", true);
             }
         }
 
         private void AlustaAluevalikko()
         {
+            if (string.IsNullOrEmpty(jsonTartunnat)) return;
             // Luodaan sanakirjat alueindekseistä ja -nimistä.
             // Tiedetään että niissä on samat avaimet.
             var alueIndeksit = JsonSerializer.Deserialize<Dictionary<string, int>>(
@@ -125,21 +167,81 @@ namespace Koronatartunnat
             // Kutsuu comboBoxAlue_SelectedIndexChanged().
             comboBoxAlue.SelectedIndex = comboBoxAlue.Items.Count - 1;
         }
-
-        private void PaivitaTartuntaluku()
+        
+        private void NaytaTartuntaluku()
         {
-            labelTartunnat.Text = tartuntatapaukset[comboBoxAlue.SelectedIndex.ToString()];
+            labelTartunnat.Text = tartuntojenLkm.ToString();
         }
 
-        private void dateTimePickerTartuntapaiva_ValueChanged(object sender, EventArgs e)
+        private void AlustaTartuntaluku()
         {
-            PaivitaTartuntatapaustiedot();
-            PaivitaTartuntaluku();
+            tartuntojenLkm = int.Parse(tartuntatapaukset[comboBoxAlue.SelectedIndex.ToString()]);
+            labelTartunnat.Text = "";
+            labelVastaus.Text = "";
         }
 
-        private void comboBoxAlue_SelectedIndexChanged(object sender, EventArgs e)
+        private void NaytaVirhe(string virheviesti, bool estaOhjaimet)
         {
-            PaivitaTartuntaluku();
+            labelVirhe.Size = new Size(labelVirhe.Size.Width, 21);
+            labelVirhe.Text = virheviesti;
+            labelVirhe.BackColor = Color.Red;
+            if (estaOhjaimet) VaihdaOhjaimienTila(false);
+        }
+
+        private void AlustaVirhe()
+        {
+            labelVirhe.Size = new Size(labelVirhe.Size.Width, 0);
+            labelVirhe.Text = "";
+            labelVirhe.BackColor = Color.Transparent;
+            VaihdaOhjaimienTila(true);
+        }
+
+        private void VaihdaOhjaimienTila(bool aktiivinen)
+        {
+            textBoxArvaus.Enabled = aktiivinen;
+            buttonArvaus.Enabled = aktiivinen;
+            buttonNayta.Enabled = aktiivinen;
+        }
+
+        private string HaeVerkosta(HttpClient c, string osoite)
+        {
+            Task<string> t = c.GetStringAsync(osoite);
+            try
+            {
+                return t.Result;
+            }
+            catch (AggregateException ae)
+            {
+                // https://docs.microsoft.com/en-us/dotnet/api/system.aggregateexception?view=net-6.0#examples
+                ae.Handle((x) =>
+                {
+                    if (x is HttpRequestException) // This we know how to handle.
+                    {
+                        NaytaVirhe("Osoitetta ei pystytty tulkitsemaan. Tarkista verkkoyhteys.", true);
+                        return true;
+                    }
+                    return false; // Let anything else stop the application.
+                });
+
+                return "";
+            }
+        }
+
+        private string ErotaOlio(string jsonYlaOlio, string jsonOlioAvain, string jsonData)
+        {
+            // Olion avaimen etsintä alkaa ylä-olion sijainnista.
+            int ylaOlionSijainti = jsonData.IndexOf(jsonYlaOlio);
+            if (ylaOlionSijainti < 0) return "";
+
+            int haetunOlionSijainti = jsonData.IndexOf(jsonOlioAvain, ylaOlionSijainti) + jsonOlioAvain.Length;
+            if (haetunOlionSijainti < ylaOlionSijainti) return "";
+
+            // Olion esittely loppuu oikeaan aaltosulkeeseen.
+            // Otetaan myös aaltosulku mukaan (+1).
+            int haetunOlionLoppuSijainti = jsonData.IndexOf('}', haetunOlionSijainti) + 1;
+            if (haetunOlionLoppuSijainti < haetunOlionSijainti) return "";
+
+            return jsonData[haetunOlionSijainti..haetunOlionLoppuSijainti];
         }
     }
 }
