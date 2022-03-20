4159f241f4220c2f30b8eb4458aeccf51da1fd42
Herman Lätti <herman.latti@gmail.com>
2022-03-20 10:48:07 +0200

    Luo Liiku-metodi pelaajalle.

--- ohjelmiston-toteutusprojekti/PuupeliKomento/PuupeliKomento/model/Pelaaja.cs
+++ ohjelmiston-toteutusprojekti/PuupeliKomento/PuupeliKomento/model/Pelaaja.cs
@@ -1,11 +1,13 @@
 ﻿namespace Puupeli.model
 {
+    internal enum Suunta { Ita, Etela, Lansi, Pohjoinen }
+
     internal class Pelaaja
     {
         internal string Nimi { get; }
-        internal int AlueX { get; set; }
-        internal int AlueY { get; set; }
-        internal int Polttopuita { get; set; }
+        internal int AlueX { get; private set; }
+        internal int AlueY { get; private set; }
+        internal int Polttopuita { get; private set; }
 
         internal Pelaaja(string nimi, int alueX, int alueY, int polttopuita)
         {
@@ -13,6 +15,28 @@
             AlueX = alueX;
             AlueY = alueY;
             Polttopuita = polttopuita;
+        }
+
+        internal bool Liiku(Suunta suunta)
+        {
+            switch (suunta)
+            {
+                case Suunta.Ita:
+                    AlueX += 1;
+                    break;
+                case Suunta.Etela:
+                    AlueY += 1;
+                    break;
+                case Suunta.Lansi:
+                    AlueX -= 1;
+                    break;
+                case Suunta.Pohjoinen:
+                    AlueY -= 1;
+                    break;
+                default:
+                    return false;
+            }
+            return true;
         }
     }
 }
