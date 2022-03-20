46521eb03659136958a42300eaa99cef3176fd91
Herman Lätti <herman.latti@gmail.com>
2022-03-20 15:07:13 +0200

    Lisää aluemalli.

---
+++ ohjelmiston-toteutusprojekti/PuupeliKomento/PuupeliKomento/model/Alue.cs
@@ -0,0 +1,15 @@
+﻿namespace Puupeli.model
+{
+    internal abstract class Alue
+    {
+        internal int SijaintiX { get; }
+        internal int SijaintiY { get; }
+        internal abstract string Tyyppi { get; }
+
+        private protected Alue(int sijaintiX, int sijaintiY)
+        {
+            SijaintiX = sijaintiX;
+            SijaintiY = sijaintiY;
+        }
+    }
+}
---
+++ ohjelmiston-toteutusprojekti/PuupeliKomento/PuupeliKomento/model/Metsa.cs
@@ -0,0 +1,14 @@
+﻿namespace Puupeli.model
+{
+    internal class Metsa : Alue
+    {
+        internal override string Tyyppi { get; }
+        internal int Puita { get; }
+
+        internal Metsa(int sijaintiX, int sijaintiY, int puita) : base(sijaintiX, sijaintiY)
+        {
+            Tyyppi = "Metsä";
+            Puita = puita;
+        }
+    }
+}
